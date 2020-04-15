47281eee6f3e2ba1145f7f8d13073128983a0853
JamesMReilly <jimreilly28@gmail.com>
2020-04-14 20:08:06 -0500

    dice rolls randomly every click

--- index.js
+++ index.js
@@ -1,42 +1,63 @@
-let state = 1;
 let cube = document.querySelector('.cube');
+let button = document.querySelector('.button')
+let isEvenRoll = true;
+let isRolling = false;
+let previousState;
 
-const rotate = () => {
-	state++;
-	if (state === 7) state = 1
-	console.log("You rolled a " + state)
+cube.addEventListener("transitionend", () => isRolling = false)
 
-	switch (state) {
-
+const random = () => Math.floor(Math.random() * 6) + 1;
+const classForN = n => {
+	switch (n) {
 		case 1:
-			cube.classList.remove('roll-six');
-			cube.classList.add('roll-one');
-			break;
+			return 'roll-one';
 
 		case 2:
-			cube.classList.remove('roll-one');
-			cube.classList.add('roll-two');
-			break;
+			return 'roll-two';
 
 		case 3:
-			cube.classList.remove('roll-two');
-			cube.classList.add('roll-three');
-			break;
+			return 'roll-three';
 
 		case 4:
-			cube.classList.remove('roll-three');
-			cube.classList.add('roll-four');
-			break;
+			return 'roll-four';
 
 		case 5:
-			cube.classList.remove('roll-four');
-			cube.classList.add('roll-five');
-			break;
+			return 'roll-five';
 
 		case 6:
-			cube.classList.remove('roll-five');
-			cube.classList.add('roll-six');
-			break;
+			return 'roll-six';
+	}
+}
 
+const roll = () => {
+	console.log(isRolling)
+
+	if (!isRolling) {
+		//block button spam
+		isRolling = true;
+
+		//roll
+		const n = random();
+		console.log("You rolled a " + n)
+
+		//remove old transform
+		if (previousState) cube.classList.remove(previousState);
+
+		//swap even odd flip for big roll animations every time
+		if (isEvenRoll) {
+			cube.classList.remove('even')
+			cube.classList.add('odd')
+		} else {
+			cube.classList.remove('odd')
+			cube.classList.add('even')
+		}
+
+		//this is a dumb way to force the dice to animate if we roll the same number twice in a row
+		isEvenRoll = !isEvenRoll;
+
+		//add new class
+		const newState = classForN(n);
+		cube.classList.add(newState);
+		previousState = newState;
 	}
 }
